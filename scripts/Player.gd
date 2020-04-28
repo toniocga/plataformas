23e3017e490e5af64ec904aa9c9fabf160b36647
brecabre <bravomantonio@gmail.com>
2020-04-28 23:17:10 +0200

    subo player gd

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,75 +1,41 @@
+
+##############
 extends KinematicBody2D
 
-const GRAVEDAD = 2000			#Valor que simula la gravedad para el eje y
-const SALTO = -400				#Cantidad de pixeles que utilizamos como fuerza para el salto
-const VELOCIDAD = 80			#Velocidad de desplazamiento lateral
-var desplazamiento = Vector2()	#Cantidad de pixeles que se desplaza en cada eje
-var mover = Vector2()			#Variable para calcular el vector de desplazamiento
-var movimiento = Vector2()		#Variable que almacena el movimiento que queda por realizar
-								#al resolverse una colisión
-var saltando = false
-
-var origen = Vector2()
-
+#onready var sprite : Sprite = $Sprite
+#onready var animation_player : AnimationPlayer = $AnimationPlayer
+#onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer2D
+export var snap := false
+export var move_speed := 250
+export var jump_force := 500
+export var gravity := 900
+export var slope_slide_threshold := 50.0
+var vidas_personaje = 3
+var velocity := Vector2()
+#var conesion_anima_fin
+var nodoprincipal
 func _ready():
-	set_fixed_process(true)
-	set_process_input(true)
-	origen = self.get_pos()
-	
-func _input(event):
-	if event.is_action_pressed("ui_up"):
-		if not saltando:
-			desplazamiento.y = SALTO
-		saltando = true
-		get_node("AnimatedSprite").play("saltar")
-	
-	
-func _fixed_process(delta):
+	nodoprincipal = get_tree().get_root().get_node("Principal")
+func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"
+	)
+	velocity.x = direction_x * move_speed
+#	velocity.x = analog_velocity.x* move_speed
+	if Input.is_action_just_pressed("ui_up") and snap and not Input.is_action_pressed("abajo"):
+		velocity.y = -jump_force
+		snap = false
+#		audio_player.play()
 
-	desplazamiento.y += GRAVEDAD * delta
+	velocity.y += gravity * delta
 
-	
-	if Input.is_action_pressed("ui_left"):
-		desplazamiento.x = -VELOCIDAD
-		if not saltando:
-			get_node("AnimatedSprite").play("caminar")
-			get_node("AnimatedSprite").set_flip_h(true)
-	elif Input.is_action_pressed("ui_right"):
-		desplazamiento.x = VELOCIDAD
-		if not saltando:
-			get_node("AnimatedSprite").play("caminar")
-			get_node("AnimatedSprite").set_flip_h(false)
-	else:
-		desplazamiento.x = 0
-		get_node("AnimatedSprite").stop()
-		get_node("AnimatedSprite").set_frame(0)
-		
-	mover = desplazamiento * delta
-	mover = move(mover)
-	
-	# Para resolver la colision del KinematicBody 2D se utiliza el siguiente código
-	if is_colliding():
-		get_node("AnimatedSprite").set_animation("caminar")
-		saltando = false
-		var n = get_collision_normal()
-		mover = n.slide(mover)
-		desplazamiento = n.slide(desplazamiento)
-		move(mover)
-		
-	if self.get_pos().x < 0:
-		self.set_pos(Vector2(0,self.get_pos().y))
-	if self.get_pos().x < 0:
-		self.set_pos(Vector2(0,self.get_pos().y))
-		
-	if self.get_pos().y > get_viewport_rect().size.y:
-		Global.vidas -= 1
-		self.set_pos(origen)
-		
-	comprobar_posicion()
-	
-func comprobar_posicion():
-	var pos_act = self.get_pos()
-	
-	self.set_pos(Vector2(clamp(pos_act.x,8,630),pos_act.y))
+	var snap_vector = Vector2(0,32) if snap else Vector2()
+	velocity = move_and_slide_with_snap(velocity,snap_vector, Vector2.UP, slope_slide_threshold)
+
+	if is_on_floor() and (Input.is_action_just_released("move_right") or Input.is_action_just_released("move_left")):
+		velocity.y = 0
+
+	var just_landed := is_on_floor() and not snap
+	if just_landed:
+		snap = true
 	
 	
