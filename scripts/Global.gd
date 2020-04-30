1b58310c5e6795ad7c1d73e64836a71fe123aa33
CycloPi <venti3d@gmail.com>
2020-04-30 19:52:59 +0200

    scrip menu

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -2,3 +2,7 @@ extends Node
 
 var puntuacion = 0
 var vidas = 3
+
+func _ready():
+	print("puntuación: ", puntuacion, ", vidas: ", vidas)
+	pass
---
+++ scripts/Menu_entrar.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var nivel0 = load("res://scenes/Level0.tscn").instance()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_TextureButton_button_down():
+	print("picncho")
+	
+	get_node("/root").add_child(nivel0)
+	queue_free()
+	
+	pass # Replace with function body.
