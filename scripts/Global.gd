98c98e588d319037031b240733c6d68d82c13f96
brecabre <bravomantonio@gmail.com>
2020-05-03 19:53:11 +0200

    pongo el exit en el global

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -7,3 +7,7 @@ var nivel = 0
 func _ready():
 	print("puntuación: ", puntuacion, ", vidas: ", vidas , ", nivel", nivel)
 	pass
+func _unhandled_input(event):
+    if event is InputEventKey:
+        if event.pressed and event.scancode == KEY_ESCAPE:
+            get_tree().quit()
