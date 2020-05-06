e78882b00f5a16dab4188e21355eaed1c4948a50
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-06 18:33:34 +0200

    el game over es a varias vidas

--- Menus/GameOver/GameOver.gd
+++ Menus/GameOver/GameOver.gd
@@ -2,14 +2,12 @@ extends Node2D
 
 
 onready var MenuPrincipal = load("res://Menus/MenuPrincipal/MenuPrincipal.tscn").instance()
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#
-#	pass # Replace with function body.
+
 
 func _on_Timer_timeout():
 	Global.puntuacion = 0
-	print(get_parent().get_parent().name)
+	Global.vidas = Global.vidasIniciales
+	Global.nivel = 0
 	get_node("/root").add_child(MenuPrincipal)
-	get_parent().get_parent().queue_free()
-	pass # Replace with function body.
+	get_parent().queue_free()
+	pass 
