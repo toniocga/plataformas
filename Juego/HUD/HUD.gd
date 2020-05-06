882d3c24bda1ace4bcf388c5fcf2d3bd137b6021
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-06 18:43:30 +0200

    quito las vidas del HUD

--- Juego/HUD/HUD.gd
+++ Juego/HUD/HUD.gd
@@ -1,15 +1,18 @@
 extends CanvasLayer
 
 
-#func _ready():
-#	set_process(true)
+
 #func puntos (puntos):
 #	$puntuacion.set_text(puntos) 
-##func _process(delta):
+func _process(delta):
 #	get_node("puntuacion").set_text(str(Global.puntuacion))
-#	if Global.vidas == 2:
-#		get_node("vidas/vida1").set_hidden(true)
-#	if Global.vidas == 1:
-#		get_node("vidas/vida2").set_hidden(true)
-#	if Global.vidas == 0:
-#		get_node("vidas/vida3").set_hidden(true)
+	if Global.vidas == 2:
+		get_node("vidas/vida3").set_visible(false)
+		
+	if Global.vidas == 1:
+		get_node("vidas/vida3").set_visible(false)
+		get_node("vidas/vida2").set_visible(false)
+	if Global.vidas == 0:
+		get_node("vidas/vida3").set_visible(false)
+		get_node("vidas/vida2").set_visible(false)
+		get_node("vidas/vida1").set_visible(false)
