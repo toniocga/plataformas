6832922954bb3439afaa4d9770b9cdacfbdcc12e
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-06 18:06:18 +0200

    arreglo pasar de pantalla

--- Menus/Win/Win.gd
+++ Menus/Win/Win.gd
@@ -1,29 +1,29 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var nivel1 = load("res://Juego/Escenarios/Niveles/Nivel_1/Nivel_1.tscn").instance()
-onready var final = load("res://Menus/Win/Win.tscn").instance()
-# Called when the node enters the scene tree for the first time.
+onready var nivel2 = load("res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn").instance()
+onready var final = load("res://Menus/Creditos/Creditos.tscn").instance()
+
+
 func _ready():
-	
-	pass # Replace with function body.
+	Global.nivel += 1
+	pass 
 
 func _on_Timer_timeout():
+	pasarPantalla()
+
+
+func pasarPantalla():
 	if Global.nivel == 0:
-
+		get_parent().get_parent().queue_free()
 		get_node("/root").add_child(nivel1)
-		Global.nivel += 1
+		
+	if Global.nivel == 1:
 		get_parent().get_parent().queue_free()
-	
-	elif Global.nivel == 1: # final
+		get_node("/root").add_child(nivel2)
+		
+	if Global.nivel == 2: 
+		get_parent().get_parent().queue_free()
 		get_node("/root").add_child(final)
-		get_parent().get_parent().queue_free()
 
-#		Global.nivel = 0 # está en el menú  entrar
-
-	pass # Replace with function body.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass 
