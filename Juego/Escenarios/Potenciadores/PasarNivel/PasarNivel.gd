2a57d408769077c163daef19fe5559339a741986
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-07 19:01:47 +0200

    pasar niveles y fin de juego

--- Juego/Escenarios/Potenciadores/PasarNivel/PasarNivel.gd
+++ Juego/Escenarios/Potenciadores/PasarNivel/PasarNivel.gd
@@ -7,11 +7,13 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
+		if get_parent().filename == "res://Juego/Escenarios/Niveles/Nivel_1/Nivel_1.tscn":
+			Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn")
+		if get_parent().filename == "res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn":
+			Global_cambiar_nivel.goto_scene("res://Menus/Creditos/Creditos.tscn")
 #		print("pbody," , body.bandera_muerto)
 #		body.bandera_muerto = true
 #		add_child(win)
 #		win.set_position(Vector2(-96,-50))
-		Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn")
-	
 		
 #	pass 
