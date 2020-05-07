4f475ce4ab16327c8076f87ce07548d87c6f0dda
brecabre <bravomantonio@gmail.com>
2020-05-07 16:10:12 +0200

    pasar nivel gd

--- Juego/Escenarios/Potenciadores/PasarNivel/PasarNivel.gd
+++ Juego/Escenarios/Potenciadores/PasarNivel/PasarNivel.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
 
-onready var win = load("res://Menus/Win/Win.tscn").instance()
+#onready var win = load("res://Menus/Win/Win.tscn").instance()
 
 
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
-		print("pbody," , body.bandera_muerto)
-		body.bandera_muerto = true
-		add_child(win)
-		win.set_position(Vector2(-96,-50))
-		
+#		print("pbody," , body.bandera_muerto)
+#		body.bandera_muerto = true
+#		add_child(win)
+#		win.set_position(Vector2(-96,-50))
+		Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_2/Nivel_2.tscn")
 	
 		
-	pass 
+#	pass 
