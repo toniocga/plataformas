a0d0cbad0a27ed072068aa02d733eb670bf6dc77
brecabre <bravomantonio@gmail.com>
2020-05-07 22:41:01 +0200

    cambio el global cambio de escena

--- recursos/Global/Global_cambiar_nivel.gd
+++ recursos/Global/Global_cambiar_nivel.gd
@@ -1,32 +1,35 @@
 extends Node
-var pant_carga
+#var pant_carga
 var loader
 var wait_frames
 var time_max = 100 # msec
 var current_scene
+onready var escena_carga
 
 func _ready():
-    var root = get_tree().get_root()
-    current_scene = root.get_child(root.get_child_count() -1)
-
+	
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() -1)
+#	root.call_deferred("add_child()","escena_carga")
 func goto_scene(path): # game requests to switch to this scene
+	escena_carga = load("res://Menus/pantalla_carga/Carga.tscn").instance()
+	add_child(escena_carga)
+	
 	loader = ResourceLoader.load_interactive(path)
 	if loader == null: # check for errors
 #        show_error()
 		return
 	set_process(true)
-	
+
 	current_scene.queue_free() # get rid of the old scene
-	pant_carga = load("res://Menus/pantalla_carga/Carga.tscn").instance()
-#	pant_carga.instance()
-	get_tree().get_root().add_child(pant_carga)
-	# start your "loading..." animation
-#    get_node("animation").play("carga")
+	
+#	node("animation").play("carga")
+	
+	wait_frames = 20
 
-	wait_frames = 40
-
-func _process(time):
+func _process(_time):
 	if loader == null:
+		
 		# no need to process anymore
 		set_process(false)
 		return
@@ -43,8 +46,12 @@ func _process(time):
 
 		if err == ERR_FILE_EOF: # Finished loading.
 			var resource = loader.get_resource()
+			
 			loader = null
+			 
+			
 			set_new_scene(resource)
+			
 			break
 		elif err == OK:
 #			update_progress()
@@ -55,17 +62,20 @@ func _process(time):
             break
 			
 #func update_progress():
-#    var progress = float(loader.get_stage()) / loader.get_stage_count()
-    # Update your progress bar?
+#	var progress = float(loader.get_stage()) / loader.get_stage_count()
+#	var length = get_node("animation").get_current_animation_length()
+		
+	# Update your progress bar?
 #    get_node("progress").set_progress(progress)
 
     # ... or update a progress animation?
-#    var length = get_node("animation").get_current_animation_length()
-
+#   
     # Call this on a paused animation. Use "true" as the second argument to force the animation to update.
 #    get_node("animation").seek(progress * length, true)
 
 func set_new_scene(scene_resource):
+	escena_carga.queue_free()
 	current_scene = scene_resource.instance()
+	
 	get_node("/root").add_child(current_scene)
-	pant_carga.queue_free()
+	
