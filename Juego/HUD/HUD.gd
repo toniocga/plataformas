bd01ea2a640519f49d62121845cce256c24397dd
CycloPi <venti3d@gmail.com>
2020-05-08 18:55:43 +0200

    quito errores hud

--- Juego/HUD/HUD.gd
+++ Juego/HUD/HUD.gd
@@ -4,15 +4,15 @@ extends CanvasLayer
 
 #func puntos (puntos):
 #	$puntuacion.set_text(puntos) 
-func _process(delta):
+func _process(_delta):
 #	get_node("puntuacion").set_text(str(Global.puntuacion))
 	if Global.vidas == 2:
 		get_node("vidas/vida3").set_visible(false)
 		
-	if Global.vidas == 1:
+	elif Global.vidas == 1:
 		get_node("vidas/vida3").set_visible(false)
 		get_node("vidas/vida2").set_visible(false)
-	if Global.vidas == 0:
+	elif Global.vidas == 0:
 		get_node("vidas/vida3").set_visible(false)
 		get_node("vidas/vida2").set_visible(false)
 		get_node("vidas/vida1").set_visible(false)
