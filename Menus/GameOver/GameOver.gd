2d3056f98c291da441f8de9e35bd4f4b16bc4a90
juanantoniogit <juanantonio.tecno@gmail.com>
2020-05-08 16:18:03 +0200

    centro la img del menu gameOver

--- Menus/GameOver/GameOver.gd
+++ Menus/GameOver/GameOver.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 onready var MenuPrincipal = load("res://Menus/MenuPrincipal/MenuPrincipal.tscn").instance()
 
+func _ready():
+	get_node("Sprite").set_position(Vector2(get_viewport().size.x/2 , get_viewport().size.y/2))
 
 func _on_Timer_timeout():
 	Global.puntuacion = 0
