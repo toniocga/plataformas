01b810b3cfc83a6f662ba3688dcb094d728578c7
brecabre <bravomantonio@gmail.com>
2020-05-23 00:45:54 +0200

    Add files via upload

--- Menus/MenuPrincipal/MenuPrincipal.gd
+++ Menus/MenuPrincipal/MenuPrincipal.gd
@@ -10,7 +10,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_TextureButton_button_down():
-	Global.nivel = 0
+	Global.nivel = 1
 #	get_node("/root").add_child(nivel1)
 #	queue_free()
 	Global_cambiar_nivel.goto_scene("res://Juego/Escenarios/Niveles/Nivel_1/Nivel_1.tscn")
