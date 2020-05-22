f80782acdb259c343a2cb39634373ead8883e621
brecabre <bravomantonio@gmail.com>
2020-05-23 00:01:37 +0200

    Add files via upload

---
+++ Juego/Escenarios/Potenciadores/armas_player/arma_01/arma_01.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+var Bullet = preload("res://Juego/Escenarios/Potenciadores/armas_player/balas/bala_01/bala_01.tscn")
+# var b = "text"
+
+func _ready():
+	
+	pass # Replace with function body.
+	
+	
+func disparo():
+	var player_dir = get_parent().get_node("AnimatedSprite").is_flipped_h()
+	var b = Bullet.instance()
+	b.set_global_position(get_parent().get_global_position())
+	get_node("/root/Level").add_child(b)
+	b.direccion_bala(player_dir)
+	pass
---
+++ Juego/Escenarios/Potenciadores/armas_player/balas/bala_01/bala_01.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+#var vidas_personaje = 3
+var velocity 
+var move_speed = 10
+var direction = Vector2(0,0)
+var obj
+onready var tilemap = get_node("/root/Level/TileMap")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.start()
+#	print(tilemap)
+
+func direccion_bala(d):
+	if d:
+		direction.x = -1
+		obj = -1
+	else:
+		direction.x = 1
+		obj = 1
+var celda_tiro 
+func _physics_process(_delta: float) -> void:
+
+	velocity = direction * move_speed
+	velocity = move_and_collide(velocity)
+	celda_tiro = tilemap.world_to_map(get_position())
+	if velocity:
+#		print(get_position())
+		
+		tilemap.set_cell(celda_tiro.x + obj  ,celda_tiro.y ,-1)
+		queue_free()
+func _on_Timer_timeout():
+	queue_free()
