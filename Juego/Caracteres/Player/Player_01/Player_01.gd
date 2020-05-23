f0e1db7a988b396eaf9ded8d293de2b762bdd328
brecabre <bravomantonio@gmail.com>
2020-05-23 20:50:02 +0200

    Add files via upload

--- Juego/Caracteres/Player/Player_01/Player_01.gd
+++ Juego/Caracteres/Player/Player_01/Player_01.gd
@@ -7,8 +7,8 @@ onready var sprite : AnimatedSprite = $AnimatedSprite
 #onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer2D
 export var snap := false
 export var move_speed := 240
-export var jump_force := 200
-export var gravity := 300
+export var jump_force := 300
+export var gravity := 600
 export var slope_slide_threshold := 50.0
 #var vidas_personaje = 3
 var velocity := Vector2()
@@ -17,52 +17,43 @@ onready var arma = get_node("arma_01")
 
 #Cyclo Pi variables
 #var bandera_boton_pulsao = false
-onready var gameover = load("res://Menus/GameOver/GameOver.tscn").instance()
+#onready var gameover = load("res://Menus/GameOver/GameOver.tscn").instance()
 var bandera_muerto = false
 var bandera_sonido_pasos = true
 
 func _physics_process(delta: float) -> void:
-	
+	print(is_on_floor())
 	var direction_x := Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
 	velocity.x = direction_x * move_speed
-	
 #	velocity.x = analog_velocity.x* move_speed
-	if Input.is_action_just_pressed("espacio") and snap and not Input.is_action_pressed("abajo"):
+	if Input.is_action_just_pressed("espacio") and snap :
+		snap = false
 		velocity.y = -jump_force
-		snap = false
-
-
+		
 #		audio_player.play()
-#	if Input.is_action_just_pressed("ui_select"):
-#		pass
-#		get_parent().PonerTile(direcciontile , 10)
-#	if Input.is_action_just_pressed("abajo"):
-#		pass
-#		get_parent().PonerTile(direcciontile , -1)
-	
-	velocity.y += gravity * delta
 
-	var snap_vector = Vector2(0,32) if snap else Vector2()
+	velocity.y += gravity * delta
+	
+	var snap_vector =  Vector2(0,32) if snap else Vector2()
 	velocity = move_and_slide_with_snap(velocity,snap_vector, Vector2.UP, slope_slide_threshold)
+#	velocity = move_and_slide(velocity,snap_vector)
+#	Vector2(0,32) if snap else Vector2()
 	
-	if not is_on_floor():
-		snap = false
-	
-	if is_on_floor() and (Input.is_action_just_released("derecha") or Input.is_action_just_released("izquierda")):
-		velocity.y = 0
+#	if is_on_floor() and (Input.is_action_just_released("derecha") or Input.is_action_just_released("izquierda")):
+#		velocity.y = 0
 
 	var just_landed := is_on_floor() and not snap
 	if just_landed:
 		snap = true
-
+#	var just_landed =  ($RayCast2D1.is_colliding() and $RayCast2D2.is_colliding()) and not snap
+#	if just_landed:
+#		snap = true
+	if not is_on_floor():
+		snap = false
 	if position.y > 170:
-#	print(position.y)
+#	
 		morir()	
-#	if Input.is_action_pressed("ui_down") and Input.is_action_just_pressed("ui_up"):
-#	print(analog_velocity.y)
-#	if analog_velocity.y < 0 and Input.is_action_just_pressed("jump"):
-#		and Input.is_action_just_pressed("jump")
-#		activar_desactivar_colision()
+#	
 	if Input.is_action_just_pressed("click_izquierdo"):
 		arma.disparo()
 			
@@ -126,6 +117,6 @@ func quitar_vida():
 	elif Global.vidas <= 1 :
 		Global.vidas -= 1
 #		Global_cambiar_nivel.wait_frames = 0
-#		Global_cambiar_nivel.goto_scene("res://Menus/GameOver/GameOver.tscn")
-		get_parent().add_child(gameover)
+		Global_cambiar_nivel.goto_scene("res://Menus/GameOver/GameOver.tscn")
+#		get_parent().add_child(gameover)
 #		add_child(gameover)
