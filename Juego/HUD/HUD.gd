60cf3ba8f1bd6b12d901ad4fb685fafd4fc1f567
juanantoniogit <juanantonio.tecno@gmail.com>
2020-06-02 18:04:27 +0200

    activa y desactiva touch

--- Juego/HUD/HUD.gd
+++ Juego/HUD/HUD.gd
@@ -16,3 +16,8 @@ func _process(_delta):
 		get_node("vidas/vida3").set_visible(false)
 		get_node("vidas/vida2").set_visible(false)
 		get_node("vidas/vida1").set_visible(false)
+	
+	if Global.touch == true:
+		get_node("touch").set_visible(true)
+	if Global.touch == false:
+		get_node("touch").set_visible(false)
