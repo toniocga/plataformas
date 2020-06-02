e5a9b9cbb71917ce6bcdc93afedfce2f372dec3f
juanantoniogit <juanantonio.tecno@gmail.com>
2020-06-02 17:57:12 +0200

    ubiccion de los botones por scritp

--- Juego/HUD/touch/touch.gd
+++ Juego/HUD/touch/touch.gd
@@ -4,11 +4,16 @@ extends Node2D
 onready var player = get_parent().get_parent().get_node("Player")
 var dere = 0
 var izq = 0
+onready var pos_controles = get_node("controles")
+onready var pos_botones = get_node("botones")
+onready var viewport_size = get_parent().get_parent().get_viewport().size
+	
 
-func _process(delta):
-	print ("dere: " + str(dere))
-	print ("izq: " + str(izq))
-	print ("resultado: " + str(dere - izq))
+
+func _ready():
+	pos_controles.set_position(Vector2(0 + 20 , viewport_size.y - 100))
+	pos_botones.set_position(Vector2(viewport_size.x - 120 , viewport_size.y - 100))
+	pass
 
 func _on_botonizq_button_down():
 	izq = 1
@@ -42,6 +47,8 @@ func _on_botonA_button_down():
 func _on_botonB_button_down():
 	player.disparo_2()
 	pass
+
+
 
 
 
